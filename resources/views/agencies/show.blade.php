3420a823cca5d48dd3eca0862d9da5b072eed17d
Rob Baartwijk <rob.baartwijk@gmail.com>
2024-11-22 00:08:47 +0100

    Stylilng

--- resources/views/agencies/show.blade.php
+++ resources/views/agencies/show.blade.php
@@ -5,7 +5,7 @@
         <div class="col-md-12">
             <div class="card ">
                 <div class="card-header">
-                    <h4 class="card-title"> Show agency</h4>
+                    <h3 class="card-title"> Show agency</h3>
                 </div>
 
                 <div class="row">
