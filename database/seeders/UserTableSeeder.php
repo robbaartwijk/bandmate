317b250361f2ed5489e13d9c6b930583fb7a8a49
Rob Baartwijk <rob.baartwijk@gmail.com>
2025-03-24 21:51:28 +0100

    Added new admin for repo.

--- database/seeders/UserTableSeeder.php
+++ database/seeders/UserTableSeeder.php
@@ -56,6 +56,36 @@ class UserTableSeeder extends Seeder
             'updated_at' => now()
         ]);
 
+        DB::table('users')->insert([
+            'id' => 3,
+            'name' => 'Test Admin',
+            'first_name' => 'Test',
+            'last_name' => 'Admin',
+            'stage_name' => 'Administrator',
+            'email' => 'admin@bandmate.online',
+            'street' => 'Kerkstraat',
+            'street_number' => '1',
+            'zip' => '1234AB',
+            'city' => 'Amsterdam',
+            'state' => 'Noord-Holland',
+            'country' => 'Netherlands',
+            'phone' => '0612345678',
+            'website' => 'https://www.admin.com',
+            'enail_notification_all' => 0,
+            'email_notification_new_act' => 1,
+            'email_notification_new_vacancy' => 1,
+            'email_notification_new_availablemusician' => 0,
+            'email_notification_new_rehearsalroom' => 1,
+            'email_notification_new_venue' => 0,
+            'email_notification_new_agency' => 0,
+            'email_notification_newsletter' => 1,
+            'email_verified_at' => now(),
+            'is_admin' => 1,
+            'password' => Hash::make('Admin99'),
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
         \App\Models\User::factory(180)->create([]);
 
     }
