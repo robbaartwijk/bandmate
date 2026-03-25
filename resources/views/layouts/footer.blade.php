0d86472c9952d838931fe3b0377edc54bea3d8ba
Rob Baartwijk <rob.baartwijk@gmail.com>
2026-03-25 11:04:56 +0100

    Updated version in footer.

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="flex-shrink-0 flex items-center justify-between px-4 sm:px-6 py-3 border-t border-white/10 text-xs text-white/30"
         style="background-color: rgb(20, 20, 70);">
-    <span><b>Version 1.2</b></span>
+    <span><b>Version 2.0</b></span>
     <span>&copy; {{ date('Y') }} Rob Baartwijk</span>
 </footer>
